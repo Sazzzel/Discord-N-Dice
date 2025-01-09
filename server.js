82ed39700c7e733542d5f027973595438cb58f87
Sazzzel <sazzzel@gmail.com>
2025-01-09 22:55:41 +0000

    Add: style to console output for character stats

--- server.js
+++ server.js
@@ -1,16 +1,5 @@
-import Die from './dice.js';
+import char from './classes/character.js';
 
-console.log('Rolling a d20...');
-const die = new Die();
-const rollResult = die.roll(8, die.dice.d20);
-console.log('Rolled:', rollResult);
+const character = new char('Hero', 15, 12, 14, 10, 8, 13, 'Warrior');
 
-// roll 8d20 10000 times and record highest roll
-let highestRoll = 0;
-for (let i = 0; i < 100000000; i++) {
-    const roll = die.roll(4, die.dice.d20);
-    if (roll > highestRoll) {
-        highestRoll = roll;
-    }
-}
-console.log('Highest roll:', highestRoll);
+console.log(character.generateStatsMessage());
